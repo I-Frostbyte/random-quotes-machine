ea11dd17243618c1d0e89764f9ecb29bcef1306e
I-Frostbyte <irefyre@gmail.com>
2022-08-12 16:40:41 +0100

    First Attempt at making it work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from "react";
+import './index.css';
+import {FaTwitter, FaTumblr} from 'react-icons/fa';
+
+class RandomQuote extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      textColor: null,
+      Quotes: null
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  fetchQuotes = async () => {
+    const request = await fetch("https://zenquotes.io/api/quotes").then((res) => {
+      console.log(res);
+      return res.json()
+    })
+    this.setState({
+      Quotes: request
+    })
+  }
+
+  newQuote() {
+    const indexQuotes = Math.floor(Math.random() * this.state.Quotes.length)
+    return this.state.Quotes[indexQuotes];
+  }
+  
+  
+  colors = ["grey", "lightgreen", "maroon", "lightblue", "red", "black", "topaz"];
+
+  newColor(colorsArray) {
+   const index = Math.floor(Math.random() * colorsArray.length)
+   return colorsArray[index];
+  } 
+  /*
+  handleClick(){
+    this.setState({
+      textColor: this.newColor(this.colors)
+    });
+  }
+  */
+ render () {
+  return (
+    <div id="quote-box">
+      <div id="content-wrapper">
+        <h1 id="text">{this.fetchQuotes}</h1>
+        <p id="author">- Random Author</p>
+        <div id="below-author">
+          <div id="icons">
+            <div id="twitter-icon">
+              <a href="twitter.com/intent/tweet" id="tweet-quote"><FaTwitter size={20} style={{color: "#fff"}} class="fas fa-twitter" /></a>
+            </div>
+            <div id="tumblr-icon">
+              <FaTumblr size={20} style={{color: "#fff"}} class="fas fa-tumblr" />
+            </div>
+          </div>
+          <div id="new-quote-button">
+            <button id="new-quote" style={{color: "#fff"}}>New Quote</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+ }
+  
+  
+}
+
+export default RandomQuote;
+
+
+
+/*
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
 export default App;
+
+*/
